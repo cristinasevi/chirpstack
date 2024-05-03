29794f3c467d094b8dc34e17271b61f3d2cea00a
cris <133981266+cristinasevi@users.noreply.github.com>
2024-05-03 11:55:45 +0200

    Add files via upload

--- device-profiles.py
+++ device-profiles.py
@@ -23,6 +23,7 @@ Comm_Threshold = config['DEFAULT'].getint('Comm_Threshold', 300)  # Convertir a 
 Log_Rate = config['DEFAULT'].getint('Log_Rate', 60)  # Convertir a entero, usar 60 por defecto si no se encuentra
 Plant_Name = config['DEFAULT'].get('Plant_Name', 'Test')  # Si no se encuentra, usar 'Test' por defecto
 
+
 def GetDeviceProfiles(server, api_token, device_id):
     url = f"http://{server}/api/device-profiles/{device_id}"
     auth_token = [("authorization", "Bearer %s" % api_token)]
@@ -44,22 +45,6 @@ if device_profiles:
     for key, value in device_profiles.items():
         print(f"{key}: {value}")
 
-def DeleteDeviceProfile(api_token, server, profile_id):
-    url = f"http://{server}/api/device-profiles/{profile_id}"
-    auth_token = [("authorization", "Bearer %s" % api_token)]
-
-    response = requests.delete(url, headers=dict(auth_token))
-
-    if response.status_code == 200:
-        print(f"El perfil de dispositivo con ID {profile_id} se eliminó correctamente.")
-    elif response.status_code == 404:
-        print(f"No se encontró un perfil de dispositivo con ID {profile_id}.")
-    else:
-        print(f"Error al eliminar el perfil de dispositivo con ID {profile_id}. Código de error: {response.status_code}")
-
-profile_id = "f31aacfd-59e8-4dcb-96ec-c9104daa6742"
-
-DeleteDeviceProfile(api_token, server, profile_id)
 
 def PostDeviceProfile(server, api_token, device_profile_data):
     url = f"http://{server}/api/device-profiles"
@@ -114,3 +99,74 @@ response = PostDeviceProfile(server, api_token, device_profile_data)
 if response:
     print("Device Profile:")
     print(response)
+
+
+def PutDeviceProfile(server, api_token, dev_eui, device_profile_data):
+    url = f"http://{server}/api/device-profiles/{dev_eui}"
+    auth_token = [("authorization", "Bearer %s" % api_token)]
+
+    response = requests.put(url, headers=dict(auth_token), json=device_profile_data)
+
+    if response.status_code == 200:
+        print(f"Perfil de dispositivo actualizado para {dev_eui}")
+    else:
+        print(f"Error al actualizar el perfil de dispositivo para {dev_eui}: {response.status_code}")
+
+dev_eui = "6bdf9a7d-cc64-4fd2-91a2-41b0a7f00c93"
+
+device_profile_data = {
+    "deviceProfile": {
+        "adrAlgorithmID": "default",
+        "classBTimeout": 0,
+        "classCTimeout": 0,
+        "factoryPresetFreqs": [],
+        "geolocBufferTTL": 0,
+        "geolocMinBufferSize": 0,
+        "macVersion": "1.0.2",
+        "maxDutyCycle": 0,
+        "maxEIRP": 14,
+        "name": "NewDeviceProfile",
+        "networkServerID": "27",
+        "organizationID": "1",
+        "payloadCodec": "CAYENNE_LPP",
+        "payloadDecoderScript": "",
+        "payloadEncoderScript": "",
+        "pingSlotDR": 0,
+        "pingSlotFreq": 0,
+        "pingSlotPeriod": 0,
+        "regParamsRevision": "RP002-1.0.2",
+        "rfRegion": "EU868",
+        "rxDROffset1": 0,
+        "rxDataRate2": 0,
+        "rxDelay1": 0,
+        "rxFreq2": 0,
+        "supports32BitFCnt": False,
+        "supportsClassB": False,
+        "supportsClassC": False,
+        "supportsJoin": True, 
+        "tags": {}, 
+        "uplinkInterval": "300s"
+    }
+}
+
+PutDeviceProfile(server, api_token, dev_eui, device_profile_data)
+
+
+def DeleteDeviceProfile(api_token, server, profile_id):
+    url = f"http://{server}/api/device-profiles/{profile_id}"
+    auth_token = [("authorization", "Bearer %s" % api_token)]
+
+    response = requests.delete(url, headers=dict(auth_token))
+
+    if response.status_code == 200:
+        print(f"El perfil de dispositivo con ID {profile_id} se eliminó correctamente.")
+    elif response.status_code == 404:
+        print(f"No se encontró un perfil de dispositivo con ID {profile_id}.")
+    else:
+        print(f"Error al eliminar el perfil de dispositivo con ID {profile_id}. Código de error: {response.status_code}")
+
+profile_id = "6bdf9a7d-cc64-4fd2-91a2-41b0a7f00c93"
+
+DeleteDeviceProfile(api_token, server, profile_id)
+
+
