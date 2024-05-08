86b22af7ef316bb1b92a10e17be0681ed24fd914
cris <133981266+cristinasevi@users.noreply.github.com>
2024-05-08 12:26:04 +0200

    Add files via upload

---
+++ curl_command.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Define el comando curl con las opciones y la URL proporcionadas
+curl_command="curl -X GET --header 'Accept: application/json' --header 'Grpc-Metadata-Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiZTc0NjBmNWUtNWNjMy00YWM3LWFkMWYtZjZlYTQ3NWYwMDlkIiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTcxNDk4Mjg0OSwic3ViIjoiYXBpX2tleSJ9.PhRDrQFKrhXWJyBkHAEyQuousmOPhCI5WOcNpK5hIbU' 'http://10.0.1.90:8080/api/devices/0004a30b00f98573/frames'"
+
+# Ejecuta el comando curl
+eval $curl_command
