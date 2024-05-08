7719a93663c438eff21246160513b97838402d80
cris <133981266+cristinasevi@users.noreply.github.com>
2024-05-08 16:58:25 +0200

    Update device_data.sh

--- device_data.sh
+++ device_data.sh
@@ -4,7 +4,7 @@
 curl_command="curl -s -X GET --header 'Accept: application/json' --header 'Grpc-Metadata-Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiZTc0NjBmNWUtNWNjMy00YWM3LWFkMWYtZjZlYTQ3NWYwMDlkIiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTcxNDk4Mjg0OSwic3ViIjoiYXBpX2tleSJ9.PhRDrQFKrhXWJyBkHAEyQuousmOPhCI5WOcNpK5hIbU' 'http://10.0.1.90:8080/api/devices/0004a30b00f98573/events'"
 
 # Ejecuta el comando curl y extrae la primera línea de la salida (el JSON devuelto)
-device_data=$(eval $curl_command | head -n 1)
+device_data=$(eval $curl_command | head -n 1 | grep -o '"data":"[^"]*' | cut -d '"' -f 4)
 
-# Imprime la primera línea de los datos del dispositivo (el JSON devuelto)
-echo "$device_data"
+# Imprime el valor de "data"
+echo "DATA: $data_value"
