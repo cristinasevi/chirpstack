f4076ab84c6b53974e4267defa539f664a96c1e1
cris <133981266+cristinasevi@users.noreply.github.com>
2024-05-20 10:06:34 +0200

    Add files via upload

--- ThingsBoard/import_device.py
+++ ThingsBoard/import_device.py
@@ -46,6 +46,7 @@ def obtener_dispositivo_por_dev_eui(dev_eui, token_chirpstack):
         return None
 device_eui_cs = "0004a30b00f98573"
 
+
 def enviar_datos_a_thingsboard(datos_dispositivo, cliente_id):
     # Obtener el token de acceso de ThingsBoard
     access_token_thingsboard = obtener_token_de_acceso_thingsboard()
@@ -77,7 +78,7 @@ def enviar_datos_a_thingsboard(datos_dispositivo, cliente_id):
             print(e)
 
 # Obtener datos del dispositivo por su dev_eui
-dev_eui_tb = "DCMgqG9gqnxueBj6qTCt"  
+dev_eui_tb = "7b07ef70-167f-11ef-994d-e3af5413ffbe"  
 
 # Obtener el token de acceso de ChirpStack
 token_chirpstack = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiZTc0NjBmNWUtNWNjMy00YWM3LWFkMWYtZjZlYTQ3NWYwMDlkIiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTcxNDk4Mjg0OSwic3ViIjoiYXBpX2tleSJ9.PhRDrQFKrhXWJyBkHAEyQuousmOPhCI5WOcNpK5hIbU"
@@ -90,4 +91,58 @@ if datos_dispositivo:
     cliente_id = "dc8383c0-f0d9-11ee-a9f5-675b85d8bd3b"
     enviar_datos_a_thingsboard(datos_dispositivo, cliente_id)
 
+
+def asignar_dispositivo_a_cliente(dev_eui, cliente_id, device_profile_id):
+    # Obtener el token de acceso de ThingsBoard
+    access_token_thingsboard = obtener_token_de_acceso_thingsboard()
+    if access_token_thingsboard:
+        # Endpoint de ThingsBoard para asignar un dispositivo a un cliente y un perfil de dispositivo
+        thingsboard_endpoint = "https://thingsboard.chemik.es:443/api/device-with-credentials"
+
+        # Datos a enviar al dispositivo
+        datos_dispositivo = {
+            "device": {
+                "name": dev_eui,
+                "type": "checkness",
+                "customerId": {
+                    "id": cliente_id,
+                    "entityType": "CUSTOMER"
+                },
+                "deviceProfileId": {
+                    "id": device_profile_id,
+                    "entityType": "DEVICE_PROFILE"
+                },
+            },
+            "credentials": {
+                "credentialsType": "ACCESS_TOKEN",
+                "credentialsId": access_token_thingsboard,
+            }
+        }
+
+        # Encabezado de autorización con el token de acceso
+        headers = {
+            "X-Authorization": f"Bearer {access_token_thingsboard}",
+            "Content-Type": "application/json"
+        }
+
+        try:
+            # Realizar la solicitud para asignar el dispositivo a un cliente y un perfil de dispositivo
+            response = requests.post(thingsboard_endpoint, json=datos_dispositivo, headers=headers)
+            
+            # Verificar el código de estado de la respuesta
+            if response.status_code == 200:
+                print("Dispositivo asignado correctamente a cliente y perfil de dispositivo en ThingsBoard.")
+            else:
+                print(f"Error al asignar dispositivo a cliente y perfil de dispositivo en ThingsBoard. Código de estado: {response.status_code}")
+                print(response.text)
+        
+        except Exception as e:
+            print("Error al conectar con ThingsBoard.")
+            print(e)
+
+# Llamar a la función para asignar el dispositivo a un cliente y un perfil de dispositivo
+dev_eui = "0004a30b00f98573"  # Reemplazar con el dev_eui correcto
+cliente_id = "dc8383c0-f0d9-11ee-a9f5-675b85d8bd3b"
+device_profile_id = "45cef3b0-2ad7-11ee-a4a5-cfc2f26174fe"  # Reemplazar con el ID correcto del perfil de dispositivo
+asignar_dispositivo_a_cliente(dev_eui, cliente_id, device_profile_id)
 
