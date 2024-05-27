fb0a39876b66a506aa5d4285ce336039d99f00c4
cris <133981266+cristinasevi@users.noreply.github.com>
2024-05-27 11:10:31 +0200

    Update backup-delete.py

--- Campo TEST/backup-delete.py
+++ Campo TEST/backup-delete.py
@@ -12,18 +12,11 @@ import csv
 
 server = "localhost:8080"
 api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiNzYyOGE4NGQtOGRhMy00ZDQ1LWJlNmYtZTM4MWY5MzQ5ZWI1IiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTY3NzY4NzI5NCwic3ViIjoiYXBpX2tleSJ9.LBx46H_nzGPkSxqsqVxU_5ig0soMF9dWlsuA6obE1EY"
-Com_Count = 0
-last_checked_day = None
-Device_List = []
-Gateway_List = []
 
 # Leer configuraciones desde config.ini
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-Comm_Threshold = config['DEFAULT'].getint('Comm_Threshold', 300)  # Convertir a entero, usar 300 por defecto si no se encuentra
-Log_Rate = config['DEFAULT'].getint('Log_Rate', 60)  # Convertir a entero, usar 60 por defecto si no se encuentra
-Plant_Name = config['DEFAULT'].get('Plant_Name', 'Test')  # Si no se encuentra, usar 'Test' por defecto
 
 def DeleteDeviceActivation(api_token, server, dev_eui):
     url = f"http://{server}/api/devices/{dev_eui}/activation"
