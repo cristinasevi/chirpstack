75c7208c47d03eb20fef727bd0d1f8b2794cefbf
cris <133981266+cristinasevi@users.noreply.github.com>
2024-06-02 18:57:18 +0200

    Update device_rssi.sh

--- Campo ARIES/device_rssi.sh
+++ Campo ARIES/device_rssi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Almacena el comando curl en una variable
-curl_command="curl -s -X GET --header \"Accept: application/json\" --header \"Grpc-Metadata-Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiZTc0NjBmNWUtNWNjMy00YWM3LWFkMWYtZjZlYTQ3NWYwMDlkIiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTcxNDk4Mjg0OSwic3ViIjoiYXBpX2tleSJ9.PhRDrQFKrhXWJyBkHAEyQuousmOPhCI5WOcNpK5hIbU\" \"http://10.0.1.90:8080/api/devices/0004a30b00f98573/frames\""
+curl_command="curl -s -X GET --header \"Accept: application/json\" --header \"Grpc-Metadata-Authorization: Bearer {{token_chirpstack}}\" \"http://localhost:8080/api/devices/{{dev_eui}}/frames\""
 
 # Ejecuta el comando curl y extrae la primera línea de la salida (el JSON devuelto)
 device_data=$(eval $curl_command | head -n 1 | grep -o '"rssi":[^,]*' | cut -d ":" -f 2)
