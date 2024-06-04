d2fdbd15f656efd8672fd17f7c119fa15b5cb63b
cris <133981266+cristinasevi@users.noreply.github.com>
2024-06-04 15:30:42 +0200

    Add files via upload

---
+++ Chirpstack v3/devices/monitor_device_status.py
@@ -0,0 +1,33 @@
+import requests
+import time
+
+server = "localhost:8080"
+api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiNzYyOGE4NGQtOGRhMy00ZDQ1LWJlNmYtZTM4MWY5MzQ5ZWI1IiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTY3NzY4NzI5NCwic3ViIjoiYXBpX2tleSJ9.LBx46H_nzGPkSxqsqVxU_5ig0soMF9dWlsuA6obE1EY"
+
+def obtener_status_devices(api_url, headers):
+    try:
+        response = requests.get(api_url, headers=headers)
+        response.raise_for_status()  
+        data = response.json()
+        return data
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+
+def actualizar_status_cada_minuto(api_url, headers):
+    while True:
+        status = obtener_status_devices(api_url, headers)
+        if status:
+            print(status)
+        time.sleep(60)  # Espera 1 minuto antes de actualizar nuevamente
+
+if __name__ == "__main__":
+    api_url = f"http://{server}/api/internal/devices/summary"
+    
+    headers = {
+        "Authorization": f"Bearer {api_token}",
+        "Content-Type": "application/json"
+    }
+    
+    actualizar_status_cada_minuto(api_url, headers)
