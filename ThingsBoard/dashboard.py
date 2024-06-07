f2d6d4b98a668e3de05aeb73835e29e1aa113619
cris <133981266+cristinasevi@users.noreply.github.com>
2024-06-07 09:33:18 +0200

    Add files via upload

--- ThingsBoard/dashboard.py
+++ ThingsBoard/dashboard.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 # Función para obtener el token de acceso del usuario en ThingsBoard
 def obtener_token_de_acceso_thingsboard():
@@ -22,79 +21,37 @@ def obtener_token_de_acceso_thingsboard():
         print(e)
         return None
 
-# Función para obtener el ID del grupo de entidades en ThingsBoard - GET /api/entityGroup/all/{ownerType}/{ownerId}/{groupType}
-def obtener_id_de_grupo_de_entidades(ownerType, ownerId, groupType):
+
+# Función para obtener información de un dashboard específico en ThingsBoard - GET /api/dashboard/info/{dashboardId}
+def obtener_informacion_del_dashboard(dashboard_id):
     token = obtener_token_de_acceso_thingsboard()
     if not token:
         return None
     
-    endpoint = f"http://thingsboard.chemik.es/api/entityGroup/all/{ownerType}/{ownerId}/{groupType}"
+    dashboard_info_endpoint = f"http://thingsboard.chemik.es/api/dashboard/info/{dashboard_id}"
     headers = {
         "Authorization": f"Bearer {token}"
     }
     
     try:
-        response = requests.get(endpoint, headers=headers)
+        response = requests.get(dashboard_info_endpoint, headers=headers)
         if response.status_code == 200:
-            grupo = response.json()
-            if 'id' in grupo and 'id' in grupo['id']:
-                return grupo['id']['id']
-            else:
-                print("La respuesta no contiene el ID esperado.")
-                print(grupo)
-                return None
+            dashboard_info = response.json()
+            return dashboard_info
         else:
-            print(f"Error al obtener el grupo de entidades. Código de estado: {response.status_code}")
+            print(f"Error al obtener la información del dashboard. Código de estado: {response.status_code}")
             print(response.text)
             return None
     except Exception as e:
-        print("Error al conectar con ThingsBoard para obtener el grupo de entidades.")
+        print("Error al conectar con ThingsBoard para obtener la información del dashboard.")
         print(e)
         return None
 
-# Función para exportar los dashboards de un grupo de entidades en ThingsBoard y guardar en un archivo JSON - GET /api/entityGroup/{entityGroupId}/dashboards/export{?limit}
-def exportar_dashboards(ownerType, ownerId, groupType, limit=None):
-    entityGroupId = obtener_id_de_grupo_de_entidades(ownerType, ownerId, groupType)
-    if not entityGroupId:
-        return None
-    
-    token = obtener_token_de_acceso_thingsboard()
-    if not token:
-        return None
-    
-    endpoint = f"http://thingsboard.chemik.es/api/entityGroup/{entityGroupId}/dashboards/export"
-    if limit:
-        endpoint += f"?limit={limit}"
-    
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
-    
-    try:
-        response = requests.get(endpoint, headers=headers)
-        if response.status_code == 200:
-            dashboards = response.json()
-            with open('dashboard.json', 'w') as file:
-                json.dump(dashboards, file, indent=4)
-            print("Dashboards exportados y guardados en 'dashboard.json'")
-            return dashboards
-        else:
-            print(f"Error al exportar los dashboards. Código de estado: {response.status_code}")
-            print(response.text)
-            return None
-    except Exception as e:
-        print("Error al conectar con ThingsBoard para exportar los dashboards.")
-        print(e)
-        return None
-
-# Ejemplo de uso de la función
-ownerType = "CUSTOMER"  
-ownerId = "dc8383c0-f0d9-11ee-a9f5-675b85d8bd3b"  
-groupType = "DASHBOARD"  
-limit = 1  # Opcional, puedes especificar el límite de dashboards a exportar
-
-dashboards_exportados = exportar_dashboards(ownerType, ownerId, groupType, limit)
-if dashboards_exportados:
-    print(json.dumps(dashboards_exportados, indent=4))
+# Ejemplo de uso
+dashboard_id = "55eb6320-0d4c-11ef-8724-b1dfb72e4ebd"  # Reemplaza con el ID del dashboard que deseas obtener
+dashboard_info = obtener_informacion_del_dashboard(dashboard_id)
+if dashboard_info:
+    print("Información del Dashboard:")
+    print(dashboard_info)
 else:
-    print("No se pudieron exportar los dashboards o hubo un error.")
+    print("No se pudo obtener la información del dashboard.")
